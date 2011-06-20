140190ccd467530d252abb5ed54b1d5469d4f85d
Strzelewicz Alexandre <strzelewicz.alexandre@gmail.com>
2011-06-20 10:11:56 +0200

    Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Soundcloud Comment Hider
+
+Automatically hide comments on Soundcloud.
+
+Link to Chrome store : https://chrome.google.com/webstore/detail/ohhonfljaahmpiekpegccelgchmnpbpk
+
+
+
---
+++ content_script.js
@@ -0,0 +1,13 @@
+/*
+ *
+ * 
+ * Soundcloud Comments Hidder
+ * by Strzelewicz Alexandre
+ * http://hemca.com
+ * v1.0
+ *
+ *
+ */
+
+function soundcloud(){$(document).ready(function(){$(".player").each(function(){$(this).children()!=undefined&&$(this).toggleClass("no-comments");})})}; soundcloud();
+
